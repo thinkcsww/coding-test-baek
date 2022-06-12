9a5280aa45b288becde4c263c0c7535cb38a96aa
sun <thinkcsw@gmail.com>
2022-06-12 18:32:18 +0900

    Go - 백준 1546

--- ct-go/main.go
+++ ct-go/main.go
@@ -3,38 +3,37 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	p1546()
+}
+
+func p1546() {
 	var reader = bufio.NewReader(os.Stdin)
 	var writer = bufio.NewWriter(os.Stdout)
 
 	defer writer.Flush()
 
-	var resultArray [42]int
-	var count = 0
+	var max float64 = 0.0
+	var total float64 = 0.0
+	var N int
+	fmt.Fscanln(reader, &N)
 
-	for i := 0; i < 10; i++ {
-		num, _ := reader.ReadString('\n')
-		num = strings.TrimSuffix(num, "\n")
-		intNum, err := strconv.Atoi(num)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		resultArray[intNum%42]++
+	for i := 0; i < N; i++ {
+		var num float64
+		fmt.Fscanf(reader, "%f", &num)
+
+		max = math.Max(max, num)
+		total += num
 	}
 
-	for _, num := range resultArray {
-		if num > 0 {
-			count++
-		}
-	}
+	fmt.Fprintf(writer, "%f", total/max*100.0/float64(N))
 
-	fmt.Fprintln(writer, count)
 }
 
 func p3052() {
