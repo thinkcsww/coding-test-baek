e208522afa0d0bfd470d3b5235a7fa4c9dc0b92f
sun <thinkcsw@gmail.com>
2022-06-15 20:40:15 +0900

    Java - 백준 10809

--- ct-java/src/com/company/Main.java
+++ ct-java/src/com/company/Main.java
@@ -8,7 +8,31 @@ import java.util.StringTokenizer;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        p11720();
+        p10809();
+    }
+
+    public static void p10809() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = bufferedReader.readLine();
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < 26; i++) {
+            arr[i] = -1;
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (arr[index] == -1) {
+                arr[index] = i;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
     }
 
     public static void p11720() throws IOException {
