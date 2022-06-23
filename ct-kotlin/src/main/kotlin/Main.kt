6883a0c537d9196193c3ef2b332cae7c8ae2500c
sun <thinkcsw@gmail.com>
2022-06-23 19:43:49 +0900

    Kotlin - 백준 2869, 1712, 10757

--- ct-kotlin/src/main/kotlin/Main.kt
+++ ct-kotlin/src/main/kotlin/Main.kt
@@ -1,9 +1,80 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.Scanner
 
 fun main(args: Array<String>) {
-    p2839()
+    p10250()
 }
+
+fun p10250() {
+    val scanner = Scanner(System.`in`)
+
+    val n = scanner.next().toInt()
+
+    for (i in 0 until n) {
+        val H = scanner.next().toInt()
+        val W = scanner.next().toInt()
+        val N = scanner.next().toInt()
+
+        var x: Int
+        var y: Int
+
+        if (N > H) {
+            x = if (N % H == 0) N / H else N / H + 1
+            y = if (N % H == 0) H else N % H
+        } else {
+            x = 1
+            y = N
+        }
+
+        if (x < 10) {
+            println("${y}0${x}")
+        } else {
+            println("${y}${x}")
+        }
+
+    }
+}
+
+fun p10757() {
+    val scanner = Scanner(System.`in`)
+
+    val A = scanner.next().toBigInteger()
+    val B = scanner.next().toBigInteger()
+
+    println(A + B)
+}
+
+fun p1712() {
+    val scanner = Scanner(System.`in`)
+    val A = scanner.next().toInt()
+    val B = scanner.next().toInt()
+    val C = scanner.next().toInt()
+
+    var result: Int
+
+    if (B >= C) {
+        result = -1
+    } else {
+        result = (A / (C - B)) + 1
+    }
+
+    println(result)
+
+}
+
+fun p2869() {
+    val scanner = Scanner(System.`in`)
+    val A: Double = scanner.next().toDouble()
+    val B: Double = scanner.next().toDouble()
+    val V: Double = scanner.next().toDouble()
+
+    val result = Math.ceil((V - A) / (A - B)).toInt()
+
+    println(result + 1)
+
+}
+
 
 fun p2839() {
     val bufferedReader = BufferedReader(InputStreamReader(System.`in`))
