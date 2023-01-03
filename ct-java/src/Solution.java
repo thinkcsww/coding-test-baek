efe222685df8742ea6f0619702b72a95aa8e9a7a
sun <thinkcsw@gmail.com>
2023-01-03 21:24:45 +0900

    Java - 프로그래머스 - 큰 수 만들기 - 그리디

--- ct-java/src/Solution.java
+++ ct-java/src/Solution.java
@@ -1,45 +1,20 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 class Solution {
-    public int solution(int n, int[][] costs) {
-        int answer = 0;
-        int[] parent = new int[n];
-        for (int i = 0; i < parent.length; i++) {
-            parent[i] = i;
-        }
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        int max = 0;
+        int index = 0;
 
-        // costs를 오름차순 정렬
-        Arrays.sort(costs, Comparator.comparingInt(cost -> cost[2]));
-
-        for (int[] cost : costs) {
-            if (union(cost[0], cost[1], parent)) {
-                answer += cost[2];
+        for (int i = 0; i < number.length() - k; i++) {
+            max = 0;
+            for (int j = index; j <= i + k ; j++) {
+                if (max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    index = j + 1;
+                }
             }
+            answer.append(max);
         }
 
-        return answer;
-    }
-
-    private boolean union(int x, int y, int[] parent) {
-        int a = find(x, parent);
-        int b = find(y, parent);
-
-        if (a != b) {
-            parent[a] = b;
-            return true;
-        }
-
-        return false;
-    }
-
-    private int find(int x, int[] parent) {
-        if (parent[x] == x) {
-            return x;
-        }
-
-        int result = find(parent[x], parent);
-        parent[x] = result;
-        return result;
+        return answer.toString();
     }
 }
