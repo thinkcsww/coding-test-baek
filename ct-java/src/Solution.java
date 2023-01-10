ccbb2006f8ec2b3695d676d781fc29bbce191d4d
sun <thinkcsw@gmail.com>
2023-01-10 21:16:55 +0900

    Java - 프로그래머스 - 카펫

--- ct-java/src/Solution.java
+++ ct-java/src/Solution.java
@@ -1,29 +1,32 @@
-import java.util.Stack;
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
 
-class Solution
-{
-    public int solution(String s)
-    {
-        int answer = 0;
+        double divider = 0;
 
-        Stack<Character> stack = new Stack<>();
+        while (true) {
+            divider++;
+            double yellowCntFor1Row = Math.ceil(yellow / divider);
 
-        for (char c: s.toCharArray()) {
-            if (stack.isEmpty()) {
-                stack.add(c);
-            } else {
-                if (stack.peek() == c) {
-                    stack.pop();
-                } else {
-                    stack.add(c);
-                }
+            //
+            double left = 0;
+            if (yellow % divider != 0) {
+                left = divider - (yellow % divider);
             }
 
+            double brownCnt = 0;
+            // 맨 위, 아래, 모서리
+            brownCnt += yellowCntFor1Row * 2 + 4;
+            // 노란색이 있는 줄의 테두리
+            brownCnt += divider * 2;
+
+            if (brown == brownCnt + left) {
+                break;
+            }
         }
 
-        if (stack.isEmpty()) {
-            answer = 1;
-        }
+        answer[0] = (int) ((brown + yellow) / (divider + 2));
+        answer[1] = (int) (divider + 2);
 
         return answer;
     }
