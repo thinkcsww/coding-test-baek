3d0c18e491a4ec3d8b2d2049254ea23f0250edf9
sun <thinkcsw@gmail.com>
2023-01-28 22:21:07 +0900

    Java - 프로그래머스 - 폰켓몬

--- ct-java/src/ProgrammerMain.java
+++ ct-java/src/ProgrammerMain.java
@@ -7,7 +7,7 @@ public class ProgrammerMain {
 
         int[][] a = new int[][]{{60, 50}, {30, 70}, {60, 30}, {80, 40}};
 
-        int solution1 = solution.solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}});
+        int solution1 = solution.solution(new int[]{3,3,3,2,2,2});
 
         System.out.println("solution1 = " + solution1);
     }
--- ct-java/src/Solution.java
+++ ct-java/src/Solution.java
@@ -1,53 +1,16 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
 
-    public int solution(int[][] maps) {
-        return bfs(maps);
-    }
-
-    private int bfs(int[][] maps) {
-        int m = maps[0].length;
-        int n = maps.length;
-
-        int[] dx = new int[]{0, 0, 1, -1};
-        int[] dy = new int[]{1, -1, 0, 0};
-
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(0, 0, 1));
-        maps[0][0] = -1;
-
-        while (!queue.isEmpty()) {
-            Point prev = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int x = prev.x + dx[i];
-                int y = prev.y + dy[i];
-
-                if (x == n - 1 && y == m - 1) {
-                    return prev.distance + 1;
-                }
-
-                if (x >= 0 && x < n && y >= 0 && y < m && maps[x][y] == 1 ) {
-                    maps[x][y] = -1;
-                    queue.add(new Point(x, y, prev.distance + 1));
-                }
+        for (int num: nums) {
+            set.add(num);
+            if (nums.length / 2 == set.size()) {
+                break;
             }
         }
-
-        return -1;
-    }
-
-    public static class Point {
-        int x;
-        int y;
-        int distance;
-
-        public Point(int x, int y, int distance) {
-            this.x = x;
-            this.y = y;
-            this.distance = distance;
-        }
+        return set.size();
     }
 }
