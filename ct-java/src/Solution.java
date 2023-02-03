3df0642c42a3f97d426fb8589160705217728c36
sun <thinkcsw@gmail.com>
2023-02-03 22:03:47 +0900

    Java - 프로그래머스 - H-Index

--- ct-java/src/Solution.java
+++ ct-java/src/Solution.java
@@ -1,61 +1,21 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
-    int[] distances;
-    boolean[] visited;
-    List<List<Integer>> adj;
-    public int solution(int n, int[][] edge) {
-        distances = new int[n + 1];
-        visited = new boolean[n + 1];
+    public int solution(int[] citations) {
+        int answer = 0;
 
-        adj = new ArrayList<>();
+        List<Integer> collect = Arrays.stream(citations)
+                .mapToObj(Integer::valueOf)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
 
-        for (int i = 0; i < n + 1; i++) {
-            adj.add(new ArrayList<>());
-        }
-
-        for (int[] e: edge) {
-            adj.get(e[0]).add(e[1]);
-            adj.get(e[1]).add(e[0]);
-        }
-
-        bfs();
-
-        int max = Arrays.stream(distances).max().getAsInt();
-        long count = Arrays.stream(distances)
-                .filter(distance -> distance == max)
-                .count();
-
-        return (int) count;
-    }
-
-    private void bfs() {
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(1, 0));
-        visited[1] = true;
-
-        while (!queue.isEmpty()) {
-            Point prev = queue.poll();
-
-            List<Integer> edges = adj.get(prev.x);
-
-            for (int edge: edges) {
-                if (!visited[edge]) {
-                    visited[edge] = true;
-                    distances[edge] = prev.distance + 1;
-                    queue.add(new Point(edge, prev.distance + 1));
+        for (int i = 1; i <= collect.size(); i++) {
+                if (collect.get(i - 1) >= i) {
+                    answer = i;
                 }
-            }
         }
-    }
 
-    private static class Point {
-        int x;
-        int distance;
-
-        public Point(int x, int distance) {
-            this.x = x;
-            this.distance = distance;
-        }
+        return answer;
     }
 }
